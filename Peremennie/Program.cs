ac84c39638e1d0f7cbc12bc46f9a08ff45529b52
Никита Митраченков <nickitaipad@gmail.com>
2021-04-03 13:27:22 +0300

    Initialize repos

---
+++ Peremennie/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peremennie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 2 числа, чтобы узнать их частное");
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            double div  = (double)number1 / number2;
+
+            Console.WriteLine("Сумма введенных чисел равна: " + div);
+
+            Console.ReadKey();
+        }
+    }
+}
